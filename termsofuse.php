d32eb64db0e361b34189e616a960662cb073d46b
mandelmaasai <mattias@moleant.com>
2022-01-28 17:50:53 +0100

    terms of use

--- termsofuse.php
+++ termsofuse.php
@@ -113,15 +113,22 @@ You may be required to register with the Site. You agree to keep your password c
 You may not access or use the Site for any purpose other than that for which we make the Site available. The Site may not be used in connection with any commercial endeavors except those that are specifically endorsed or approved by us.</p>
 
 <p>
-As a user of the Site, you agree not to:<br>
-Systematically retrieve data or other content from the Site to create or compile, directly or indirectly, a collection, compilation, database, or directory without written permission from us.
-Trick, defraud, or mislead us and other users, especially in any attempt to learn sensitive account information such as user passwords.</p>
+As a user of the any Moleant product (such as Moleant Discovery subscription), you agree not to:</p>
 <p>
-Circumvent, disable, or otherwise interfere with security-related features of the Site, including features that prevent or restrict the use or copying of any Content or enforce limitations on the use of the Site and/or the Content contained therein.
-Disparage, tarnish, or otherwise harm, in our opinion, us and/or the Site.
-Use any information obtained from the Site in order to harass, abuse, or harm another person.
-Make improper use of our support services or submit false reports of abuse or misconduct.
-Use the Site in a manner inconsistent with any applicable laws or regulations.
+- Systematically retrieve data or other content from the Site to create or compile, directly or indirectly, a collection, compilation, database, or directory without written permission from us.</p>
+<p>
+- Trick, defraud, or mislead us and other users, especially in any attempt to learn sensitive account information such as user passwords.</p>
+<p>
+- Circumvent, disable, or otherwise interfere with security-related features of the Site, including features that prevent or restrict the use or copying of any Content or enforce limitations on the use of the Site and/or the Content contained therein.</p>
+<p>
+- Disparage, tarnish, or otherwise harm, in our opinion, us and/or the Site.</p>
+<p>
+- Use any information obtained from the Site in order to harass, abuse, or harm another person.</p>
+<p>
+- Make improper use of our support services or submit false reports of abuse or misconduct.</p>
+<p>
+- Use the Site in a manner inconsistent with any applicable laws or regulations.</p>
+<p>
 Engage in unauthorized framing of or linking to the Site.
 Upload or transmit (or attempt to upload or to transmit) viruses, Trojan horses, or other material, including excessive use of capital letters and spamming (continuous posting of repetitive text), that interferes with any party’s uninterrupted use and enjoyment of the Site or modifies, impairs, disrupts, alters, or interferes with the use, features, functions, operation, or maintenance of the Site.
 Engage in any automated use of the system, such as using scripts to send comments or messages, or using any data mining, robots, or similar data gathering and extraction tools.
@@ -133,10 +140,13 @@ Harass, annoy, intimidate, or threaten any of our employees or agents engaged in
 Attempt to bypass any measures of the Site designed to prevent or restrict access to the Site, or any portion of the Site.
 Copy or adapt the Site’s software, including but not limited to Flash, PHP, HTML, JavaScript, or other code.
 Except as permitted by applicable law, decipher, decompile, disassemble, or reverse engineer any of the software comprising or in any way making up a part of the Site.
-Except as may be the result of standard search engine or Internet browser usage, use, launch, develop, or distribute any automated system, including without limitation, any spider, robot, cheat utility, scraper, or offline reader that accesses the Site, or using or launching any unauthorized script or other software.
-Use a buying agent or purchasing agent to make purchases on the Site.
-Make any unauthorized use of the Site, including collecting usernames and/or email addresses of users by electronic or other means for the purpose of sending unsolicited email, or creating user accounts by automated means or under false pretenses.
-Use the Site as part of any effort to compete with us or otherwise use the Site and/or the Content for any revenue-generating endeavor or commercial enterprise.
+<p>
+- Except as may be the result of standard search engine or Internet browser usage, use, launch, develop, or distribute any automated system, including without limitation, any spider, robot, cheat utility, scraper, or offline reader that accesses the Site, or using or launching any unauthorized script or other software.
+Use a buying agent or purchasing agent to make purchases on the Site.</p>
+<p>
+- Make any unauthorized use of the Site, including collecting usernames and/or email addresses of users by electronic or other means for the purpose of sending unsolicited email, or creating user accounts by automated means or under false pretenses.</p>
+<p>
+- Use the Site as part of any effort to compete with us or otherwise use the Site and/or the Content for any revenue-generating endeavor or commercial enterprise.
 </div>
 
 <div class="moleantdiv">
