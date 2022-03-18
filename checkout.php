142ccbc92e97965f3d11697ec55a69c5eec07ac9
mandelmaasai <mattias@moleant.com>
2022-03-18 21:48:21 +0100

    country picker

--- checkout.php
+++ checkout.php
@@ -182,16 +182,6 @@ Subscription. Monthly billed.
 </div>
 </div>
 
-<div class=moleantdiv>
-
-<p style="font-size:8px">
-  By clicking "Agree and subscribe", you agree: You will be charged US$99 (plus tax) monthly and at the end of your one-year term, your subscription will automatically renew monthly until you cancel (price subject to change). No annual commitment required after the first year. Cancel anytime via Moleant Account or Customer Support. Cancel before Jan 22, 2022 to get a full refund and avoid a fee. You also agree to the Terms of Use and the Subscription and Cancellation Terms.
-
-</p>
-  
-<input type="button" class="button" onclick="CreateAccount(event,document.getElementById('fname').value,document.getElementById('mname').value,document.getElementById('lname').value,document.getElementById('PurchasedSubscriptions').value,document.getElementById('email').value)" value="Agree and subscribe"></button>
-</form>
-</div>
 
 <script>
   		Vue.use(VueToast, {
