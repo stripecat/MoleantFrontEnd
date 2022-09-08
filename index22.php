dc535682b5262ef34717d6ec86312a6438ec8937
mandelmaasai <mattias@moleant.com>
2022-09-08 21:40:45 +0200

    enbedded youtube

--- index22.php
+++ index22.php
@@ -363,11 +363,10 @@ $description= $item->description;
 
 <div class="column3" >
                     
-                    <div class="bg-img5" style="height:100%">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/y9L-fSgakU0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                    
                    
 
-</div>
 </div>
 
 <div class="column3" >
